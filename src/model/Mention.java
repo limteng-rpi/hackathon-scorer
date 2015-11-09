1dab50c9c5949d931d041e9fe77a6b9ea9521942
limteng <liny9@rpi.edu>
2015-11-09 15:51:31 -0800

    create

---
+++ src/model/Mention.java
@@ -0,0 +1,144 @@
+package model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mention {
+    private String text;
+    private int startOffset, endOffset;
+
+    private String annotator;
+    private String id;
+    private String doc;
+    private String translation;
+    private String type;
+    private String wiki;
+
+    private Map<String, Object> attributeMap = new HashMap<String, Object>();
+
+    /** CONSTRUCTORS */
+    public Mention() {
+
+    }
+
+    public Mention(String text, int startOffset, int endOffset) {
+        this.text         = text;
+        this.startOffset = startOffset;
+        this.endOffset   = endOffset;
+    }
+
+    /** TEXT */
+    public void text(String text) {
+        this.text = text;
+    }
+
+    public String text() {
+        return text;
+    }
+
+    /** OFFSET */
+    public void setStartOffset(int startOffset) {
+        this.startOffset = startOffset;
+    }
+
+    public void setEndOffset(int endOffset) {
+        this.endOffset = endOffset;
+    }
+
+    public int getStartOffset() {
+        return startOffset;
+    }
+
+    public int getEndOffset() {
+        return endOffset;
+    }
+
+    /** OTHER ATTRIBUTES */
+    public void setAnnotator(String annotator) {
+        this.annotator = annotator;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDoc(String doc) {
+        this.doc = doc;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setWiki(String wiki) {
+        this.wiki = wiki;
+    }
+
+    public String getAnnotator() {
+        return annotator;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDoc() {
+        return doc;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getWiki() {
+        return wiki;
+    }
+
+    /** ATTRIBUTES MAP */
+    public boolean hasAttribute(String key) {
+        return attributeMap.containsKey(key);
+    }
+
+    public Object getAttribute(String key) {
+        return attributeMap.get(key);
+    }
+
+    public void putAttribute(String key, Object value) {
+        attributeMap.put(key, value);
+    }
+
+    public boolean removeAttribute(String key) {
+        if (attributeMap.containsKey(key)) {
+            attributeMap.remove(key);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        String doc      = ((Mention) o).getDoc();
+        int startOffset = ((Mention) o).getStartOffset();
+        int endOffset   = ((Mention) o).getEndOffset();
+        return startOffset == this.startOffset && endOffset == this.endOffset && doc.equals(this.doc);
+    }
+
+    public boolean match(Mention men) {
+        return men.getStartOffset()   == this.startOffset
+                && men.getEndOffset() == this.endOffset
+                && men.getType().equals(this.type)
+                && men.getWiki().equals(this.wiki)
+                && men.getTranslation().equals(this.translation);
+    }
+}
---
+++ src/work/Evaluation.java
@@ -0,0 +1,272 @@
+package work;
+
+import model.Mention;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Evaluation {
+    private static List<String> brokenLines   = new ArrayList<String>();
+    private static List<String> repeatedLines = new ArrayList<String>();
+
+    /** Scorer
+     * @param args args[0] annotation file 1: gold
+     *             args[1] annotation file 2: result
+     */
+    public static void evaluate(String[] args)  {
+        String line;
+        NumberFormat formatter;
+        BufferedReader br1, br2;
+        List<Mention> mentionList1, mentionList2;
+        Set<String> lineSet1, lineSet2;
+
+        try {
+            int lineNum;
+            formatter = new DecimalFormat("#0.00");
+            mentionList1 = new ArrayList<Mention>();
+            mentionList2 = new ArrayList<Mention>();
+            lineSet1     = new HashSet<String>();
+            lineSet2     = new HashSet<String>();
+            br1 = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "utf-8"));
+            br2 = new BufferedReader(new InputStreamReader(new FileInputStream(args[1]), "utf-8"));
+
+            /* read gold */
+            lineNum = 0;
+            while ((line = br1.readLine()) != null) {
+                lineNum++;
+
+                if (lineSet1.contains(line)) {
+                    repeatedLines.add("file 1, line " + lineNum + ": " + line);
+                    continue;
+                } else {
+                    lineSet1.add(line);
+                }
+
+                String[] segs = line.split("\t");
+
+                if (segs.length != 7) {
+                    brokenLines.add("file 1, line " + lineNum + ": " + line);
+                    continue;
+                }
+
+                try {
+                    String annotator = segs[0];
+                    String id        = segs[1];
+                    String text      = segs[2];
+                    String doc       = segs[3].substring(0, segs[3].indexOf(':'));
+                    int startOffset  = Integer.parseInt(segs[3].substring(segs[3].indexOf(':') + 1, segs[3].indexOf('-')));
+                    int endOffset    = Integer.parseInt(segs[3].substring(segs[3].indexOf('-') + 1));
+                    String trans     = segs[4];
+                    String wiki      = segs[5];
+                    String type      = segs[6];
+
+                    Mention mention = new Mention(text, startOffset, endOffset);
+                    mention.setAnnotator(annotator);
+                    mention.setId(id);
+                    mention.setDoc(doc);
+                    mention.setTranslation(trans);
+                    mention.setWiki(wiki);
+                    mention.setType(type);
+                    mentionList1.add(mention);
+                } catch (Exception ex) {
+                    brokenLines.add("file 1, line " + lineNum + ": " + line);
+                }
+            }
+            br1.close();
+
+            /* read result */
+            lineNum = 0;
+            while ((line = br2.readLine()) != null) {
+                lineNum++;
+
+                if (lineSet2.contains(line)) {
+                    repeatedLines.add("file 2, line " + lineNum + ": " + line);
+                    continue;
+                } else {
+                    lineSet2.add(line);
+                }
+
+                String[] segs = line.split("\t");
+
+                if (segs.length != 7) {
+                    brokenLines.add("file 2, line " + lineNum + ": " + line);
+                    continue;
+                }
+
+                try {
+                    String annotator = segs[0];
+                    String id        = segs[1];
+                    String text      = segs[2];
+                    String doc       = segs[3].substring(0, segs[3].indexOf(':'));
+                    int startOffset  = Integer.parseInt(segs[3].substring(segs[3].indexOf(':') + 1, segs[3].indexOf('-')));
+                    int endOffset    = Integer.parseInt(segs[3].substring(segs[3].indexOf('-') + 1));
+                    String trans     = segs[4];
+                    String wiki      = segs[5];
+                    String type      = segs[6];
+
+                    Mention mention = new Mention(text, startOffset, endOffset);
+                    mention.setAnnotator(annotator);
+                    mention.setId(id);
+                    mention.setDoc(doc);
+                    mention.setTranslation(trans);
+                    mention.setWiki(wiki);
+                    mention.setType(type);
+                    mentionList2.add(mention);
+                } catch (Exception ex) {
+                    brokenLines.add("file 2, line " + lineNum + ": " + line);
+                }
+            }
+            br2.close();
+
+            /* identification f-score */
+            int menNum1 = mentionList1.size();
+            int menNum2 = mentionList2.size();
+            int correctPositiveNum = 0;
+            for (Mention men2 : mentionList2) {
+                if (mentionList1.contains(men2)) {
+                    correctPositiveNum++;
+                }
+            }
+            double idPrecision;
+            double idRecall;
+            double idFScore;
+            if (menNum2 == 0) {
+                idPrecision = 0.0;
+            } else {
+                idPrecision = (double) correctPositiveNum / menNum2;
+            }
+            if (menNum1 == 0) {
+                idRecall = 0.0;
+            } else {
+                idRecall = (double) correctPositiveNum / menNum1;
+            }
+            if (idPrecision + idRecall < 10e-6) {
+                idFScore = 0.0;
+            } else {
+                idFScore = 2.0 * (idPrecision * idRecall) / (idPrecision + idRecall);
+            }
+            System.out.println("ID Precision:      " + formatter.format(idPrecision * 100.0) + "%");
+            System.out.println("ID Recall:         "  + formatter.format(idRecall * 100.0) + "%");
+            System.out.println("ID F-score:        " + formatter.format(idFScore * 100.0) + "%");
+
+
+            /* type accuracy */
+            int correctTypeNum = 0;
+            for (Mention men2 : mentionList2) {
+                if (mentionList1.contains(men2)) {
+                    if (men2.getType().equals(mentionList1.get(mentionList1.indexOf(men2)).getType())) {
+                        correctTypeNum++;
+                    }
+                }
+            }
+            double typeAccuracy;
+            if (correctPositiveNum == 0) {
+                typeAccuracy = 0.0;
+            } else {
+                typeAccuracy = (double) correctTypeNum / correctPositiveNum;
+            }
+            System.out.println("Type Accuracy:     " + formatter.format(typeAccuracy * 100.0) + "%");
+
+
+            /* translation accuracy */
+            int correctTransNum = 0;
+            for (Mention men2 : mentionList2) {
+                if (mentionList1.contains(men2)) {
+                    if (men2.getTranslation().equals(mentionList1.get(mentionList1.indexOf(men2)).getTranslation())) {
+                        correctTransNum++;
+                    }
+                }
+            }
+            double transAccuracy;
+            if (correctPositiveNum == 0) {
+                transAccuracy = 0.0;
+            } else {
+                transAccuracy = (double) correctTransNum / correctPositiveNum;
+            }
+            System.out.println("Trans Accuracy:    " + formatter.format(transAccuracy * 100.0) + "%");
+
+            /* wikipedia accuracy */
+            int correctWikiNum = 0;
+            for (Mention men2 : mentionList2) {
+                if (mentionList1.contains(men2)) {
+                    if (men2.getWiki().equals(mentionList1.get(mentionList1.indexOf(men2)).getWiki())) {
+                        correctWikiNum++;
+                    }
+                }
+            }
+            double wikiAccuracy;
+            if (correctPositiveNum == 0) {
+                wikiAccuracy = 0.0;
+            } else {
+                wikiAccuracy = (double) correctWikiNum / correctPositiveNum;
+            }
+            System.out.println("Wiki Accuracy:     " + formatter.format(wikiAccuracy * 100.0) + "%");
+
+            /* overall */
+            int exactlyMatchNum = 0;
+            for (Mention men2 : mentionList2) {
+                if (mentionList1.contains(men2)) {
+                    if (men2.match(mentionList1.get(mentionList1.indexOf(men2)))) {
+                        exactlyMatchNum++;
+                    }
+                }
+            }
+            double overallPrecision;
+            double overallRecall;
+            double overallFScore;
+            if (menNum2 == 0) {
+                overallPrecision = 0.0;
+            } else {
+                overallPrecision = (double) exactlyMatchNum / menNum2;
+            }
+            if (menNum1 == 0) {
+                overallRecall = 0.0;
+            } else {
+                overallRecall = (double) exactlyMatchNum / menNum1;
+            }
+            if (overallPrecision + overallRecall < 10e-6) {
+                overallFScore = 0.0;
+            } else {
+                overallFScore = 2.0 * (overallPrecision * overallRecall) / (overallPrecision + overallRecall);
+            }
+            System.out.println("Overall Precision: " + formatter.format(overallPrecision * 100.0) + "%");
+            System.out.println("Overall Recall:    " + formatter.format(overallRecall * 100.0) + "%");
+            System.out.println("Overall F-score:   " + formatter.format(overallFScore * 100.0) + "%");
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        /* for testing */
+        // String[] testArgs = {"path_to_file_1", "path_to_file_2"};
+        // args = testArgs;
+
+        /* check arguments */
+        if (args.length != 2) {
+            System.out.println("ERROR: Invalid arguments\n" +
+                    "USAGE: java -jar eval.jar [annotation_file_1] [annotation_fle_2]\n" +
+                    "annotation_file_1: gold\nannotation_file_2: result");
+            return;
+        }
+
+        /* run scorer */
+        evaluate(args);
+
+        /* print broken lines */
+        if (brokenLines.size() != 0) {
+            System.out.println("\nBroken lines:");
+            for (String line : brokenLines) {
+                System.out.println(line);
+            }
+        }
+    }
+}
